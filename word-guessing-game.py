f687299c171dcec9cf5a271e6fa99c0e1883037b
Siya Gladile <siya.gladile@gmail.com>
2024-06-19 16:15:46 +0200

    first commit

---
+++ word-guessing-game.py
@@ -0,0 +1,6 @@
+#ask user for their username
+print("What is your name?")
+username = input()
+
+print(f"Welcome to the word guessing game {username} . You have 12 turns to guess the word that has been randomly selected for you. Goodluck {username}!!!")
+
